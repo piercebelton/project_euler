afad85d5920b9b413dd2c041e8ab14ec6cc71662
piercebelton <piercebelton@gmail.com>
2017-02-12 15:51:11 -0800

    completed problem 15

--- project_euler.js
+++ project_euler.js
@@ -477,9 +477,23 @@ longestCollatzSequence();
 // Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 // How many such routes are there through a 20×20 grid?
 
-function routesInAGrid(){
-
+function latticePaths(gridSize) {
+  var moves = gridSize * 2;
+  var gridFactorial = 1;
+  var movesFactorial = 1;
+  var numOfPaths;
+  for (i = gridSize; i > 0; i--) {
+    gridFactorial *= i;
+  }
+  for (j = moves; j > 0; j--) {
+    movesFactorial *= j;
+  }
+  console.log(movesFactorial);
+  console.log(gridFactorial);
+  numOfPaths = movesFactorial / gridFactorial / gridFactorial;
+  console.log(numOfPaths);
 }
+latticePaths(20);     
 
 
 
@@ -489,10 +503,59 @@ function routesInAGrid(){
 
 
 
+// Problem 17:
+// If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+// If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+// Note: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
 
 
+// Problem 18:
+// By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+//
+// 3
+// 7 4
+// 2 4 6
+// 8 5 9 3
+//
+// That is, 3 + 7 + 4 + 9 = 23.
+//
+// Find the maximum total from top to bottom of the triangle below:
+//
+// 75
+// 95 64
+// 17 47 82
+// 18 35 87 10
+// 20 04 82 47 65
+// 19 01 23 75 03 34
+// 88 02 77 73 07 63 67
+// 99 65 04 28 06 16 70 92
+// 41 41 26 56 83 40 80 70 33
+// 41 48 72 33 47 32 37 16 94 29
+// 53 71 44 65 25 43 91 52 97 51 14
+// 70 11 33 28 77 73 17 78 39 68 17 57
+// 91 71 52 38 17 14 91 43 58 50 27 29 48
+// 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+// 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+//
+// NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 
+
+
+function alphabetPosition(text) {
+  var strValue = ""
+  for (i = 0; i < text.length; i++) {
+    if (!isNaN(parseInt(text.charAt(i), 36) - 9)) {
+    strValue = strValue.concat(text.charAt(i).charCodeAt() -96) + " ");
+    }
+  }
+  var newStr = strValue.substring(0, strValue.length-1);
+  return newStr;
+}
+
+alphabetPosition("hello")
+
+'a'.charCodeAt() - 96; // 1
 
 
 
